355e9fd673fc8fb2c252ad33045f937020a6fbea
EA4FRB <melchor.varela@gmail.com>
2018-04-06 17:54:59 +0200

    New installer

---
+++ publish/win32/publish-win32.bat
@@ -0,0 +1,7 @@
+md packages\com.vendor.product\data
+md ..\..\installer\win32
+copy ..\..\build-analyzer-Desktop_Qt_5_10_1_MinGW_32bit-Release\release\analyzer.exe packages\com.vendor.product\data\analyzer.exe
+C:\Qt\5.10.1\mingw53_32\bin\windeployqt packages\com.vendor.product\data\analyzer.exe
+copy "C:\Qt\5.10.1\mingw53_32\bin\libstdc++-6.dll" packages\com.vendor.product\data\
+C:\Qt\Tools\QtInstallerFramework\3.0\bin\binarycreator --offline-only -c config\config.xml -p packages ..\..\installer\win32\setup.exe
+pause
